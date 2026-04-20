4d0fc30deb6439825c963df9f226f36d2e7353d0
yc603 <rachel0603rachel@gmail.com>
2026-04-20 17:17:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -103,12 +103,18 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(45,1,0.1,1000);
 camera.position.set(0,3.2,8.5);   // 拉远镜头
 
+const container = document.getElementById("cakeWrap");
+
 const renderer = new THREE.WebGLRenderer({
-alpha:true,
-antialias:true
+    alpha:true,
+    antialias:true
 });
 
-renderer.setSize(320,320);
+renderer.setSize(320, 320);
+renderer.setPixelRatio(window.devicePixelRatio);
+
+container.innerHTML = "";
+container.appendChild(renderer.domElement);
 
 document.getElementById("cake3d").innerHTML="";
 document.getElementById("cakeWrap").appendChild(renderer.domElement);
